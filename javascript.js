f71014ae29aec1fdb4a8193938e457492c84c249
Graham <git@grahamirvine.com>
2022-03-22 15:18:38 -0600

    Created grid and basic page style
    Started with basic CSS to create header, footer, div container and other
    layout.
    
    Set CSS attributes for etch a sketch boxes.
    
    Created a loop in javascript to create div element and append inside
    container to create a 16 * 16 grid.

---
+++ javascript.js
@@ -0,0 +1,15 @@
+//create a div box element
+//append to container box
+//loop until it is 16 * 16
+
+const container = document.querySelector('.container');
+const etchBox = document.createElement('div');
+etchBox.setAttribute('class', 'etchBox');
+
+container.appendChild(etchBox);
+ 
+for (let i = 0; i < 16 * 16 -1; i++) {
+  let etchBox = document.createElement('div');
+  etchBox.setAttribute('class', 'etchBox');
+  container.appendChild(etchBox);
+}
