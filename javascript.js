778a1a4839fde3556dd31f384f529bf7f42ced0d
Graham <git@grahamirvine.com>
2022-03-23 15:56:39 -0600

    added a clearbox fucntion to wipe gride

--- javascript.js
+++ javascript.js
@@ -15,8 +15,39 @@ for (let i = 0; i < 16 * 16 -1; i++) {
 }
 
 const boxes = document.querySelectorAll('.etchBox');
-boxes.forEach((box) => {
-  box.addEventListener('mouseover', () => {
-  box.style.backgroundColor = 'blue';
+
+container.addEventListener('click', drawing);
+
+function drawing() {
+  boxes.forEach((box) => {
+    box.addEventListener('mouseover', () => {
+    box.style.backgroundColor = 'blue';
+    });
   });
-});
+}
+      
+
+const reset = document.querySelector('.resetBtn');
+
+reset.addEventListener('click', clearBoxes);
+
+function clearBoxes() {
+  boxes.forEach((box) => {
+    box.style.backgroundColor = 'white';
+  });
+  let gridSize;
+  do {
+    gridSize = parseInt(window.prompt('How many squares per side of the grid (max: 100)?', ''));
+    gridSize = parseInt(gridSize);
+  } while(isNaN(gridSize) || gridSize > 100 || gridSize < 1);
+  drawGrid(gridSize);
+}
+
+function drawGrid(num) {
+  for (let i = 0; i < num * num -1; i++) {
+    let etchBox = document.createElement('div');
+    etchBox.setAttribute('class', 'etchBox');
+    container.appendChild(etchBox);
+  }
+}
+
