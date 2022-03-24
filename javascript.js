a5400b59841812532afb4fade672bf4805c783c3
Graham <git@grahamirvine.com>
2022-03-23 19:40:53 -0600

    added drawgrid function but doesnt currently work

--- javascript.js
+++ javascript.js
@@ -13,14 +13,15 @@ for (let i = 0; i < 16 * 16 -1; i++) {
   etchBox.setAttribute('class', 'etchBox');
   container.appendChild(etchBox);
   }
-
-  //this isn't working if I remove it in css
-//boxes.forEach((box) => {
-//  box.style.flexBasis = 100/16;
-//  box.style.height = 100/16;
-//});
-
+  
 const boxes = document.querySelectorAll('.etchBox');
+  //this isn't working if I remove it in css
+boxes.forEach((box) => {
+  box.style.flexBasis = 100/16*100;
+  box.style.height = 100/16*100;
+});
+
+
 container.addEventListener('click', drawing);
 
 function drawing() {
@@ -42,8 +43,7 @@ function clearBoxes() {
   });
   let gridSize;
   do {
-    gridSize = parseInt(window.prompt('How many squares per side of the grid (max: 100)?', ''));
-    gridSize = parseInt(gridSize);
+    gridSize = parseInt(window.prompt('How many squares per side of the grid (max: 100)? (starting grid is 16)', ''));
   } while(isNaN(gridSize) || gridSize > 100 || gridSize < 1);
   removeBoxes();
   //drawGrid(gridSize);
@@ -54,11 +54,11 @@ function removeBoxes() {
     box.remove());  
   }
 
-//function drawGrid(num) {
-//  for (let i = 0; i < num * num -1; i++) {
-//    let etchBox = document.createElement('div');
-//    etchBox.setAttribute('class', 'etchBox');
-//    container.appendChild(etchBox);
-//  }
-//} // need to set container height and width automatically based on box size. 
-// also need to set flex basis automatically
+function drawGrid(num) {
+  for (let i = 0; i < num * num -1; i++) {
+    let etchBox = document.createElement('div');
+    etchBox.setAttribute('class', 'etchBox');
+    container.appendChild(etchBox);
+  }
+} // need to set container height and width automatically based on box size. 
+ //also need to set flex basis automatically
