bbf99bf16f93fc3c0991a9a5fc129e3688e106d5
Graham <git@grahamirvine.com>
2022-03-26 16:54:29 -0600

    rgbToHsl now converts rgb to hsl

--- script.js
+++ script.js
@@ -53,23 +53,24 @@ const rgbToHsl = (color) => {
   let b = parseInt(rgb[2]);
   console.log('r = ' + r, 'g = ' + g, 'b = ' + b);
   console.log(typeof(r));
-  r /= 255;
-  g /= 255;
-  b /= 255;
-  const l = Math.max(r, g, b);
-  const s = l - Math.min(r, g, b);
-  const h = s
-    ? l === r
-      ? (g - b) / s
-      : l === g
-      ? 2 + (b - r) / s
-      : 4 + (r - g) / s
-    : 0;
-  return [
-    60 * h < 0 ? 60 * h + 360 : 60 * h,
-    100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0),
-    (100 * (2 * l - s)) / 2,
-  ];
+  r /= 255, g /= 255, b /= 255;
+  let max = Math.max(r, g, b), min = Math.min(r, g, b);
+  let h, s, l = (max + min) / 2;
+
+  if(max == min){
+      h = s = 0; // achromatic
+  }else{
+      let d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+      switch(max){
+          case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+          case g: h = (b - r) / d + 2; break;
+          case b: h = (r - g) / d + 4; break;
+      }
+      h /= 6;
+  }
+  console.log([h, s, l]);
+  return [h, s, l];
 
 };
 
